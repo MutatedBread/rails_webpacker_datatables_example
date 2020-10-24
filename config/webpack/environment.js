f70433d6636706feb2c9aa5fcc45b240219b546b
mutatedbread <mutatedbread@gmail.com>
2020-10-24 21:26:46 +0800

    add webpack loaders

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,20 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
+const webpack = require("webpack");
 
-module.exports = environment
+const datatables = require("./loaders/datatables");
+const jquery = require("./loaders/jquery");
+const styles = require("./loaders/styles");
+
+environment.loaders.append("datatables", datatables);
+environment.loaders.append("expose", jquery);
+environment.loaders.append("styles", styles);
+
+environment.plugins.append(
+    "ProviderPlugin",
+    new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+    })
+);
+
+module.exports = environment;
