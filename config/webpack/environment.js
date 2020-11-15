0025fc1f19e184a81a805197c21d7e4c9befe870
mutatedbread <mutatedbread@gmail.com>
2020-11-15 16:05:17 +0800

    add in the comments

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,21 @@
 const { environment } = require("@rails/webpacker");
 const webpack = require("webpack");
 
+// import out loaders
 const datatables = require("./loaders/datatables");
 const jquery = require("./loaders/jquery");
 const styles = require("./loaders/styles");
 
+// append them to webpack loaders.
 environment.loaders.append("datatables", datatables);
 environment.loaders.append("expose", jquery);
 environment.loaders.append("styles", styles);
 
+// ProviderPlugin helps us to load jQuery when the variables of $ and jQuery
+// are encountered as free variables at other modules.
+// Let's say if you want to use Bootstrap 4 and Popper.js.
+//
+// Refer here https://webpack.js.org/plugins/provide-plugin/
 environment.plugins.append(
     "ProviderPlugin",
     new webpack.ProvidePlugin({
--- config/webpack/loaders/jquery.js
+++ config/webpack/loaders/jquery.js
@@ -1,3 +1,6 @@
+// expose-loader helps us to expose the jquery module as $ and jQuery at
+// the global object, allowing us to access it at Rails server-rendered
+// views.
 module.exports = {
     test: require.resolve("jquery"),
     loader: "expose-loader",
